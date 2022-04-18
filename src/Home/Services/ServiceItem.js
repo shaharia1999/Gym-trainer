4d076c7dab0f9c995f0f9381f21faa92870a33fd
Shaharia Ahmed <adeptshaharia@gmail.com>
2022-04-18 14:34:06 +0600

    channe-item-name

--- src/Home/Services/ServiceItem.js
+++ src/Home/Services/ServiceItem.js
@@ -7,7 +7,7 @@ const ServiceItem = ({item,func}) => {
     return (
         <div className='Service-item'>
             <img src={picture} alt=""  />
-             <h2>Benefites :{name.slice(0,20)}...</h2>
+             <h2>Name :{name.slice(0,20)}...</h2>
              <h2>Price :{price}</h2>
              <p>Description :{description.slice(0,100)}..</p>
              <button className='bg-primary b-none py-2 px-5 rounded-pill text-white' onClick={()=>func(item)}>Detelas</button>
